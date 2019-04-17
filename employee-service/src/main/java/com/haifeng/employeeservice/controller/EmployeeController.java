94728486010fef36e96315355cdf75ef3d3a2f87
hfchen <haifeng.chen@windstoneservice.com>
2019-04-17 16:58:57 +0800

    add employee-service

---
+++ employee-service/src/main/java/com/haifeng/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,50 @@
+package com.haifeng.employeeservice.controller;
+
+import com.haifeng.employeeservice.model.Employee;
+import com.haifeng.employeeservice.repository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeRepository repository;
+
+    @PostMapping
+    public Employee add(@RequestBody Employee employee) {
+        log.info("Employee add: {}", employee);
+        return repository.add(employee);
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Long id) {
+        log.info("Employee find: id={}", id);
+        return repository.findById(id);
+    }
+
+    @GetMapping
+    public List findAll() {
+        log.info("Employee find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public List findByDepartment(@PathVariable("departmentId") Long departmentId) {
+        log.info("Employee find: departmentId={}", departmentId);
+        return repository.findByDepartment(departmentId);
+    }
+
+    @GetMapping("/organization/{organizationId}")
+    public List findByOrganization(@PathVariable("organizationId") Long organizationId) {
+        log.info("Employee find: organizationId={}", organizationId);
+        return repository.findByOrganization(organizationId);
+    }
+
+}
